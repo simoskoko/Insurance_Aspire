28b43ae0a6f32b3cf8a675ef4c1856801bff90ae
Simo <112989400+simoskoko@users.noreply.github.com>
2026-03-30 23:22:46 +0200

    front

---
+++ OsiguranjeAspire.Contracts/Korisnik/Auth.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OsiguranjeAspire.Contracts.Korisnik
+{
+    public sealed class LoginRequestDto
+    {
+        public string Username { get; set; } = "";
+        public string Password { get; set; } = "";
+    }
+
+    public sealed class LoginResponseDto
+    {
+        public string Token { get; set; } = "";
+        public DateTime ExpiresAtUtc { get; set; }
+    }
+}
